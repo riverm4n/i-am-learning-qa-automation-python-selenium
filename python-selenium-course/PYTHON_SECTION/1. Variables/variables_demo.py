d9d41ce6d045ba4f6988024f944b01b07c61f1f9
Mário Hirotoshi Sugai Júnior <mariohiro@MacBook-Air-de-Mario.local>
2023-02-27 21:48:09 -0400

    First commit in a long time. Starting a QA Automation course with Python+Selenium Webdriver. Let's see where it goes

---
+++ python-selenium-course/PYTHON_SECTION/1. Variables/variables_demo.py
@@ -0,0 +1,19 @@
+# First script that I'm writing for this course. This is a pretty basic lesson, this course goes from the very basics of
+# Python language and then attempts to build your knowledge in order to know how to automate tests using Python +
+# Selenium Webdriver. It's been 2 years since I really programmed seriously so let's rock.
+
+# Variable definitions
+name = "Mario Hirotoshi"
+title = "Jr. QA Engineer"
+field = "Computer Science"
+age = 25
+
+# printing section
+print("====================")
+print("Your name is: " + name)
+print("Your title currently is: " + title)
+print("Your age nowadays is:" + str(age))
+
+print(
+    "{} is really trying to practice code again, he's a {} and it's been so long since he last programmed that is getting kind of ridiculous.".format(
+        name, title))
